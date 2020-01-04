12d8482187a19d7a0d25ce3071befae766d81f88
Tugay Kılavuzoğlu <tkilavuzoglu@bimser.com.tr>
2020-01-04 10:56:35 +0300

    test projesinin silinmesi

--- AppNet.WebApp/Global.asax.cs
+++ AppNet.WebApp/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,6 +15,12 @@ namespace AppNet.WebApp
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
